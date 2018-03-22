29fc0960d91e34c99743714eed61824d6fdabe73
madhurarambhajani89 <37583732+madhurarambhajani89@users.noreply.github.com>
2018-03-22 17:41:32 +0530

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,37 @@
+#Question1
+first_name = 'Mr. Kyle Walker'
+second_name = 'Mrs. Irina Shayk'
+sliced_first_name =first_name[4:15]
+print(sliced_first_name)
+sliced_second_name =second_name[5:16]
+print(sliced_second_name)
+
+#question2
+current_nameplate='Mr.AlanWalkar'
+hero='JamesBond'
+end_replacement=hero[5:9]
+start_replacement=current_nameplate[0:7]
+new =start_replacement+end_replacement
+print (new)
+
+#question3
+runs = 68
+balls = 55
+target = 192
+print('Sachin scored %d runs in %d balls to set a target of %d' %(runs,balls,target))
+
+
+#question4
+brand_name = "Fast and Furious"
+sequel_number = 9
+
+movie_name = brand_name + str(sequel_number)
+
+print (movie_name)
+
+#question5
+vehicle = 'Honda City'
+mileage = 22
+unit = 'km/l'
+print('My %s has a mileage of %d%s ' % (vehicle, mileage, unit))
+
